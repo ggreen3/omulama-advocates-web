15e407c6cda403442f42497b98a2a11f096552d0
ggreen3 <144909377+ggreen3@users.noreply.github.com>
2025-04-04 15:38:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-//import { componentTagger } from "lovable-tagger";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/ggreen3/",
-});
-
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -15,8 +11,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    //mode === 'development' &&
-    //componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
